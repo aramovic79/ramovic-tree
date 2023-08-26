b00c9667d8c34ee6c0395648f1a1bf62691eca1a
bilolkobilov <bilol.kobilov@s.amity.edu>
2023-08-27 00:45:21 +0500

    something

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'family-tree';
+  familyMembers: any[] = [];
+  newMember: any = {};
+
+  possibleRelationships = ['Child', 'Spouse', 'Sibling'];
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  addFamilyMember() {
+    if (this.newMember.name && this.newMember.relationship) {
+      this.familyMembers.push({ ...this.newMember });
+      this.newMember = {};
+    }
+  }
+
+  
 }
